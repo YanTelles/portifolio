03e894378184be6d13e7be4e08d99bd69a07e7fb
YanTelles <48263268+YanTelles@users.noreply.github.com>
2021-04-14 18:52:18 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,3 @@
-/*===== MENU SHOW Y HIDDEN =====*/ 
 const navMenu = document.getElementById('nav-menu'),
       toggleMenu = document.getElementById('nav-toggle'),
       closeMenu = document.getElementById('nav-close')
